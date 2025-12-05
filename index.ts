806dedf31ae1c0df17d73dab4967cdf1c4ef9fb3
subikcnc <subik@curvesncolors.ch>
2025-12-05 14:16:45 +0545

    AI Chat with memory completed

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
+import { addMessages, getMessages } from './src/memory'
 
 const userMessage = process.argv[2]
 
@@ -7,6 +8,21 @@ if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
-const response = await runLLM({ userMessage })
+
+await addMessages([
+    { role: 'user', content: userMessage },
+])  
+const messages = await getMessages()
+
+const response = await runLLM({messages})
+
+if(!response) {
+    console.error('No response from LLM')
+    process.exit(1)
+}
+
+await addMessages([
+    { role: 'assistant', content: response},
+])
 
 console.log(response)
--- types.ts
+++ types.ts
@@ -1,6 +1,7 @@
 export type AIMessage =
   | { role: 'user'; content: string }
   | { role: 'tool'; content: string; tool_call_id: string }
+  | {role: 'assistant'; content: string}
 
 export interface ToolFn<A = any, T = any> {
   (input: { userMessage: string; toolArgs: A }): Promise<T>
