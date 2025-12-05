7ca133b373b20ac6041fbb913be1499685c01888
subikcnc <subik@curvesncolors.ch>
2025-12-05 13:38:20 +0545

    Created a file database to act as a memory for the llm

--- src/llm.ts
+++ src/llm.ts
@@ -1,11 +1,12 @@
+import type { AIMessage } from '../types'
 import { groq } from './groqai'
 
-export const runLLM = async ({ userMessage }: { userMessage: string }) => {
+export const runLLM = async ({messages}: {messages: AIMessage[]}) => {
   // Call the openai completion
   const response = await groq.chat.completions.create({
     model: 'llama-3.1-8b-instant',
     temperature: 0.1, // How creative you want the model to be, reduces the randomness for less number
-    messages: [{ role: 'user', content: userMessage }], // Array of objects that represent messages in this chat
+    messages, // Array of objects that represent messages in this chat
   })
 
   return response.choices[0].message.content
